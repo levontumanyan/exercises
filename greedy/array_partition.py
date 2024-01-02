0ce50435f43ff1d52320288fbc526d200857cd5a
itfibonacci <levon.tumanyan@outlook.com>
2024-01-02 11:42:00 -0500

    easy greedy leetcode

---
+++ greedy/array_partition.py
@@ -0,0 +1,29 @@
+"""
+Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn) such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
+
+Example 1:
+
+Input: nums = [1,4,3,2]
+Output: 4
+Explanation: All possible pairings (ignoring the ordering of elements) are:
+1. (1, 4), (2, 3) -> min(1, 4) + min(2, 3) = 1 + 2 = 3
+2. (1, 3), (2, 4) -> min(1, 3) + min(2, 4) = 1 + 2 = 3
+3. (1, 2), (3, 4) -> min(1, 2) + min(3, 4) = 1 + 3 = 4
+So the maximum possible sum is 4.
+Example 2:
+
+Input: nums = [6,2,6,5,1,2] Options: (6, 2) (6, 5) (1, 2) = 2 + 5 + 1 = 8
+Output: 9
+Explanation: The optimal pairing is (2, 1), (2, 5), (6, 6). min(2, 1) + min(2, 5) + min(6, 6) = 1 + 2 + 6 = 9.
+"""
+
+def array_partition(nums):
+	sorted_nums = sorted(nums)
+
+	tupple_nums = sum(min(sorted_nums[i], sorted_nums[i+1]) for i in range(0, len(sorted_nums), 2))
+
+	return tupple_nums
+	
+
+	
+print(array_partition([6,2,6,5,1,2]))
