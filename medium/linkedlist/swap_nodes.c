ae79931600db138c784681a5a6847d65501dc525
itfibonacci <levon.tumanyan@outlook.com>
2024-05-10 19:01:38 -0400

    swap nodes linkedlist

--- medium/linkedlist/swap_nodes.c
+++ medium/linkedlist/swap_nodes.c
@@ -39,12 +39,38 @@ struct ListNode* swapNodes(struct ListNode* head, int k) {
 	return head;
 }
 
+/*
+First initiate two pointers at head slow and fast, then start iterating the fast pointer up to the k-th element save the pointer to this k-th node, once reached start the slow pointer that is still pointing to head, iterate it until the fast pointer reaches to the last node. then the slow one will be at n-kth element, then swap.
+*/
+struct ListNode *swapNodesSimple(struct ListNode *head, int k) {
+	struct ListNode *slow = head;
+	struct ListNode *fast = head;
+
+	while (k > 1) {
+		fast = fast->next;
+		k--;
+	}
+
+	struct ListNode *kth = fast;
+
+	while (fast != NULL) {
+		fast = fast->next;
+		slow = slow->next;
+	}
+
+	int temp = kth->val;
+	kth->val = slow->val;
+	slow->val = temp;
+
+	return head;
+}
+
 void test_swap_nodes() {
 	// Test case 1: Normal case with multiple zeroes and numbers in between
 	int arr[] = {7,9,6,6,7,8,3,0,9,5};
 	struct ListNode* head1 = createlinkedlistfromarray(arr, sizeof(arr) / sizeof(arr[0]));
 	printlinkedlist(head1);
-	printlinkedlist(swapNodes(head1, 5));
+	printlinkedlist(swapNodesSimple(head1, 2));
 	//assert(compareLinkedLists(result1, [3, 7]));
 }
 
