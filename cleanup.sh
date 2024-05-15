2840131ac011daacc831611038f1c1f82320c339
itfibonacci <levon.tumanyan@outlook.com>
2024-05-15 11:25:42 -0400

    cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Check if argument is a directory
+if [[ ! -d $1 ]]; then
+    echo "Error: Argument is not a directory"
+    exit 1
+fi
+
+# Delete binary files without an extension and print the list of deleted files
+find $1 -type f ! -name "*.*" ! -path "*/.git/*" -print | while read file; do
+    if [[ $(file --mime-encoding "$file") != *ascii* ]]; then
+        echo "$file"
+        rm -f "$file"
+    fi
+done
